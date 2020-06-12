361927d5ee22f572636b93edfab791e91ed9a2bf
Arnab Roy <trifiasco@gmail.com>
2020-06-12 20:20:25 +0600

    Add built files 12-06-2020 08:15

---
+++ build/precache-manifest.e65dc9fc179b9a336334a1c863c25d94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec53df120f7eb964a400cba7844522f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d472c90f921eb50fda7",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c0e5562ecbfd5c51093c",
+    "url": "/static/js/2.df7debec.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.df7debec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d472c90f921eb50fda7",
+    "url": "/static/js/main.824334e0.chunk.js"
+  },
+  {
+    "revision": "bb9996faae0e517a57dd",
+    "url": "/static/js/runtime-main.cca2996b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
